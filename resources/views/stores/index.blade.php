c5253a3c2455638850138121ae47a3970e3c8366
Harvey Ramos <harvey@harvsworld.com>
2016-01-17 12:56:10 -0500

    Fix wrong ID

--- resources/views/stores/index.blade.php
+++ resources/views/stores/index.blade.php
@@ -23,7 +23,7 @@
                                 <a class="dropdown-item" href="{{ route('stores.edit', $store->id) }}">Edit</a>
                                 <a class="dropdown-item" href="{{ route('stores.destroy', $store->id) }}">Delete</a>
                                 <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="{{ route('ordersStores', $order->store->id) }}">Orders</a>
+                                <a class="dropdown-item" href="{{ route('ordersStores', $store->id) }}">Orders</a>
                             </div>
                         </div>
                         <div class="card-block">
