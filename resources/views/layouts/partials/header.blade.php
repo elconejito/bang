5d6de53aa9904326726cfbe78647add68eba3d99
Harvey Ramos <harvey@harvsworld.com>
2017-08-06 23:58:15 -0400

    Update menu to Bootstrap 4

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -1,25 +1,31 @@
-<nav class="navbar navbar-static-top navbar-dark bg-inverse">
+<nav class="navbar navbar-toggleable-sm navbar-static-top navbar-inverse bg-inverse">
     <div class="container">
-        <a class="navbar-brand" href="{{ url('/') }}">Bang</a>
-        <ul class="nav navbar-nav">
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('cartridges.index') }}">Cartridges</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('ranges.index') }}">Ranges</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('stores.index') }}">Stores</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('firearms.index') }}">Firearms</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('orders.index') }}">Orders</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('trips.index') }}">Range Trips</a>
-            </li>
-        </ul>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <span class="navbar-brand" href="{{ url('/') }}">Bang</span>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('cartridges.index') }}">Cartridges</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('ranges.index') }}">Ranges</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('stores.index') }}">Stores</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('firearms.index') }}">Firearms</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('orders.index') }}">Orders</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('trips.index') }}">Range Trips</a>
+                </li>
+            </ul>
+        </div>
     </div>
 </nav>
