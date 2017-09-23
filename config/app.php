cfec8b25ac222d34b2e8d4f0345c3a3bf2bbb34a
Harvey Ramos <harvey@harvsworld.com>
2017-09-23 14:28:12 -0400

    Add laravel-ide-helper

--- config/app.php
+++ config/app.php
@@ -177,6 +177,7 @@ return [
         App\Providers\RouteServiceProvider::class,
         Barryvdh\Debugbar\ServiceProvider::class,
         Watson\Active\ActiveServiceProvider::class,
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
 
     ],
 
