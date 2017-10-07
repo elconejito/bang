29b216c07a47cfeb30f8dccfdb00321eaa8b8893
Harvey Ramos <harvey@harvsworld.com>
2017-10-07 15:25:51 -0400

    Clean up route file (sorta)

--- routes/web.php
+++ routes/web.php
@@ -15,27 +15,15 @@ Route::get('/', ['as' => 'home', function () {
     return view('welcome');
 }]);
 
-Route::resource('firearms', 'FirearmController');
-
-Route::resource('magazines', 'MagazineController');
-
-Route::resource('targets', 'TargetController');
-
-Route::resource('purposes', 'PurposeController');
-
-Route::resource('ranges', 'RangeController');
-
-Route::resource('stores', 'StoreController');
-
-Route::resource('cartridges', 'CartridgeController');
-
-
 /*
 |--------------------------------------------------------------------------
 | Magazine Routes
 |--------------------------------------------------------------------------
 */
-Route::post('magazines/{magazine}/photos', 'MagazineController@addPhoto');
+Route::resource('magazines', 'MagazineController');
+Route::prefix('magazines')->group(function () {
+    Route::post('{magazine}/photos', 'MagazineController@addPhoto');
+});
 
 
 /*
@@ -96,19 +84,21 @@ Route::resource('orders.inventories', 'InventoryController');
 | Shoot Routes
 |--------------------------------------------------------------------------
 */
-// Filtered routes
-Route::get('shoots/ranges/{id}', [
-    'as' => 'shootsRanges',
-    'uses' => 'ShootController@showRanges'
-]);
-Route::get('shoots/firearms/{id}', [
-    'as' => 'shootsFirearms',
-    'uses' => 'ShootController@showFirearms'
-]);
-Route::get('shoots/bullets/{id}', [
-    'as' => 'shootsBullets',
-    'uses' => 'ShootController@showBullets'
-]);
+Route::prefix('shoots')->group(function () {
+    // Filtered routes
+    Route::get('ranges/{id}', [
+        'as' => 'shootsRanges',
+        'uses' => 'ShootController@showRanges'
+    ]);
+    Route::get('firearms/{id}', [
+        'as' => 'shootsFirearms',
+        'uses' => 'ShootController@showFirearms'
+    ]);
+    Route::get('bullets/{id}', [
+        'as' => 'shootsBullets',
+        'uses' => 'ShootController@showBullets'
+    ]);
+});
 // Resource routes
 Route::resource('trips.shoots', 'ShootController');
 
@@ -133,3 +123,16 @@ Route::resource('trips/{trip}/targets', 'TargetController', [
     ]
 ]);
 Route::resource('trips', 'TripController');
+
+
+/*
+|--------------------------------------------------------------------------
+| Resource Routes
+|--------------------------------------------------------------------------
+*/
+Route::resource('firearms', 'FirearmController');
+Route::resource('targets', 'TargetController');
+Route::resource('purposes', 'PurposeController');
+Route::resource('ranges', 'RangeController');
+Route::resource('stores', 'StoreController');
+Route::resource('cartridges', 'CartridgeController');
