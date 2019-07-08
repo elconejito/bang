9f49b679d4d7916763fac378ccb0a23814d8534a
Harvey Ramos <harvey@harvsworld.com>
2019-07-07 23:20:08 -0400

    Update class to use Bootstrap's new float-right instead of pull-right

--- resources/views/layouts/partials/page-header.blade.php
+++ resources/views/layouts/partials/page-header.blade.php
@@ -2,7 +2,7 @@
     <div class="col page-header">
         {!! Breadcrumbs::render($breadcrumbName, $breadcrumbParams) !!}
         @if( $hasButton )
-        <a href="{{ $buttonLink }}" class="btn btn-outline-success pull-right"><i class="fas fa-plus-circle"></i> {{ $buttonText }}</a>
+        <a href="{{ $buttonLink }}" class="btn btn-outline-success float-sm-right"><i class="fas fa-plus-circle"></i> {{ $buttonText }}</a>
         @endif
         <h1>{!! $pageTitle !!}</h1>
     </div>
