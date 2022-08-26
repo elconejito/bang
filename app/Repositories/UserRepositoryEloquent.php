ed9c6c184430cc56d0cc8c6da688ea037b7b81e9
elconejito <harvey@harvsworld.com>
2022-08-26 11:03:41 -0400

    Add new User Repository for "normal" usage not related to auth

---
+++ app/Repositories/UserRepositoryEloquent.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Repositories;
+
+use Prettus\Repository\Eloquent\BaseRepository;
+use Prettus\Repository\Criteria\RequestCriteria;
+use App\Repositories\Interfaces\UserRepository;
+use App\Models\User;
+use Prettus\Repository\Exceptions\RepositoryException;
+
+/**
+ * Class UserRepositoryEloquent.
+ *
+ * @package namespace App\Repositories;
+ */
+class UserRepositoryEloquent extends BaseRepository implements UserRepository
+{
+    /**
+     * Specify Model class name
+     *
+     * @return string
+     */
+    public function model(): string
+    {
+        return User::class;
+    }
+
+
+
+    /**
+     * Boot up the repository, pushing criteria
+     * @throws RepositoryException
+     */
+    public function boot()
+    {
+        $this->pushCriteria(app(RequestCriteria::class));
+    }
+
+}
